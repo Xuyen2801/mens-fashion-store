9126aad2f146ab1bc1f9a0df5905aacf9a1bb574
TranYen-Roise <tranthihaiyen210805@gmail.com>
2026-02-24 14:11:41 +0700

    Hoàn thành task 2,3,4,5,6 mục Home, có data ảo và hình ảnh

---
+++ src/components/home/HeroBanner/HeroBanner.tsx
@@ -0,0 +1,55 @@
+import styles from "./HeroBanner.module.css";
+// import banners from "../../../data/Banner/homeStore";
+import {useState, useEffect} from "react";
+
+interface Props{
+    images: string[];
+}
+
+function HeroBanner({images}: Props){
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    useEffect(() =>{
+        const interval = setInterval(() =>{
+            setCurrentIndex((prev) =>
+                prev === images.length - 1 ? 0 : prev + 1);
+        }, 3000);
+
+        return() => clearInterval(interval);
+    }, [images.length]);
+
+    return (
+  <div className={styles.hero}>
+    <img
+      src={images[currentIndex]}
+      alt={`Banner ${currentIndex + 1}`}
+      className={styles.image}
+    />
+
+    <button
+      className={`${styles.navBtn} ${styles.prev}`}
+      onClick={() =>
+        setCurrentIndex(
+          currentIndex === 0 ? images.length - 1 : currentIndex - 1
+        )
+      }
+    >
+      &#10094;
+    </button>
+
+    <button
+      className={`${styles.navBtn} ${styles.next}`}
+      onClick={() =>
+        setCurrentIndex(
+          currentIndex === images.length - 1 ? 0 : currentIndex + 1
+        )
+      }
+    >
+      &#10095;
+    </button>
+  </div>
+);
+
+}
+
+export default HeroBanner;
---
+++ src/data/Banner/homeStore.ts
@@ -0,0 +1,7 @@
+const homeStore =[
+    "/images/header/homeStore1.png",
+    "/images/header/homeStore2.png",
+    // "/images/header/homeStore3.jpg",
+]
+
+export default homeStore;
