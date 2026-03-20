683ddc496d432e6b35043e7fb542eba83b51b7cc
unknown <23699101.vinh@student.iuh.edu.vn>
2026-03-20 14:17:15 +0700

    header

--- src/app/Gallery/page.tsx
+++ src/app/Gallery/page.tsx
@@ -1,9 +1,5 @@
 import RetroSports from "../../components/Gallery/RetroSports";
 
 export default function GalleryPage() {
-  return (
-    <main>
-      <RetroSports />
-    </main>
-  );
+  return <RetroSports />;
 }
